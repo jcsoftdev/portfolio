df2ea841ae215a2974fe100506417329de94ec5c
juancarlos <ajcarlos032@gmail.com>
2020-02-11 08:46:05 -0500

    quitando evento click a las particulas

--- js/app.js
+++ js/app.js
@@ -1,110 +1,110 @@
 particlesJS({
-  "particles": {
-    "number": {
-      "value": 53,
-      "density": {
-        "enable": true,
-        "value_area": 800
+  particles: {
+    number: {
+      value: 53,
+      density: {
+        enable: true,
+        value_area: 800
       }
     },
-    "color": {
-      "value": "#ffffff"
+    color: {
+      value: "#ffffff"
     },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#ffffff"
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#ffffff"
       },
-      "polygon": {
-        "nb_sides": 5
+      polygon: {
+        nb_sides: 5
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
       }
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
       }
     },
-    "size": {
-      "value": 12.02559045649142,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 3,
-        "size_min": 0.1,
-        "sync": false
+    size: {
+      value: 12.02559045649142,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 3,
+        size_min: 0.1,
+        sync: false
       }
     },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
     },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "bottom",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": true,
-        "rotateX": 4971.630142283318,
-        "rotateY": 2525.2724532232724
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "bottom",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 200,
+        rotateY: 200
       }
     }
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble"
       },
-      "onclick": {
-        "enable": true,
-        "mode": "bubble"
+      onclick: {
+        enable: false,
+        mode: "bubble"
       },
-      "resize": true
+      resize: true
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
         }
       },
-      "bubble": {
-        "distance": 207.079689136843,
-        "size": 16.241544246026905,
-        "duration": 0.9744926547616141,
-        "opacity": 8,
-        "speed": 3
+      bubble: {
+        distance: 200,
+        size: 12,
+        duration: 2,
+        opacity: 8,
+        speed: 3
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
+      repulse: {
+        distance: 200,
+        duration: 0.4
       },
-      "push": {
-        "particles_nb": 4
+      push: {
+        particles_nb: 4
       },
-      "remove": {
-        "particles_nb": 2
+      remove: {
+        particles_nb: 2
       }
     }
   },
-  "retina_detect": true
+  retina_detect: false
 });
